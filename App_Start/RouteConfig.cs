e80bf1beb55af263815516123a5b8e40d8cdec68
Hunter Miller <mycalhunter@gmail.com>
2021-10-05 21:28:43 -0400

    Add project files.

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace First
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // Routes are handled one after another, if one does not fit - then it will move to the next to evaluate the request
+            routes.MapRoute(
+                name: "Student",
+                url: "students/{id}/{name}/{standardId}",
+                defaults: new { controller = "Student", action = "Index", id = UrlParameter.Optional, name = UrlParameter.Optional, standardId = UrlParameter.Optional },
+                constraints: new { id = @"\d+" } // Constraint that requires ID to be numeric, non-numeric value will then be handled be a different route or if no matching routes exist -- then "The resource could not be found" error will be thrown
+            ); 
+            
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using First.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace First.Controllers
+{
+    public class StudentController : Controller
+    {
+        static IList<Student> studentList = new List<First.Models.Student>{ //First.Models.Student comes from Index.cshtml in View/Student
+                new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+                new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 }
+            };
+        // GET: Student
+        public ActionResult Index()
+        {
+            //fetch students from the DB using Entity Framework here
+
+            return View(studentList);
+        }
+    }
+}
